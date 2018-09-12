095013a61705d96e2cc62dac21922450b3de7f06
Eagleye <Eagleye@EagleEye>
2018-09-12 00:33:08 -0400

    Thread_Atomic_Types

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=atomic_types
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/atomic_types
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=atomictypes.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/atomictypes.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=atomic_types
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/atomic_types
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=atomictypes.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/atomictypes.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
